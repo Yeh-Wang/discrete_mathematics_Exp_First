338460f15822f178afd63ef70836b9cf83f4f092
Yeh-Wang <wyx1627082750@163.com>
2022-10-20 18:10:32 +0800

    YehWang

--- src/binRelationEntity.java
+++ src/binRelationEntity.java
@@ -4,13 +4,7 @@ public class binRelationEntity {  //创建实体存储序偶集(relation)
     private String sep;
     private int two;
     private String re;
-    public binRelationEntity() {
-        pr="<";
-        one=0;
-        sep=",";
-        two=0;
-        re=">";
-    }
+    public binRelationEntity() {pr="<";one=0;sep=",";two=0;re=">";}
     public int getOne() {return one;}
     public void setOne(int one) {this.one = one;}
     public int getTwo() {return two;}
--- src/proInterface.java
+++ src/proInterface.java
@@ -2,40 +2,59 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class proInterface {
-    public void showPanel(){
-        Scanner in= new Scanner(System.in);
-        ArrayList<colEntity> A = new ArrayList<>();  //A集合
-        ArrayList<colEntity> B = new ArrayList<>();  //B集合
-        ArrayList<binRelationEntity> BR = new ArrayList<>(); //A->B的关系
-        String gather;
-        System.out.println("请输入集合A:(格式为“<,>");
-        gather=in.nextLine();
-        for(int i=1;i<gather.length();i=i+2)
-        {
-            colEntity data1 =new colEntity();
-            data1.setData(Integer.parseInt(gather.substring(i,i+1)));
-            A.add(data1);
+    ArrayList<colEntity> A = new ArrayList<>();  //A集合
+    ArrayList<colEntity> B = new ArrayList<>();  //B集合
+    ArrayList<binRelationEntity> BR = new ArrayList<>(); //A->B的关系
+
+    public void showPanel() {
+        int j = 0;
+        while (j < 99) {
+            Scanner in = new Scanner(System.in);
+            String gather;
+            System.out.println("请输入集合A:(格式为“<,>");
+            gather = in.nextLine();
+            for (int i = 1; i < gather.length(); i = i + 2) {
+                colEntity data1 = new colEntity();
+                data1.setData(Integer.parseInt(gather.substring(i, i + 1)));
+                A.add(data1);
+            }
+            in.reset();
+            System.out.println("请输入集合B:(格式为“<,>");
+            gather = in.nextLine();
+            for (int i = 1; i < gather.length(); i = i + 2) {
+                colEntity data2 = new colEntity();
+                data2.setData(Integer.parseInt(gather.substring(i, i + 1)));
+                B.add(data2);
+            }
+            in.reset();
+            System.out.println("请输入A到B的关系:(输入格式为序偶集)");
+            gather = in.nextLine();
+            for (int i = 2; i < gather.length(); i = i + 6) {
+                binRelationEntity data = new binRelationEntity();
+                data.setOne(Integer.parseInt(gather.substring(i, i + 1)));
+                data.setTwo(Integer.parseInt(gather.substring(i + 2, i + 3)));
+                BR.add(data);
+            }
+            j++;
         }
-        in.reset();
-        System.out.println("请输入集合B:(格式为“<,>");
-        gather=in.nextLine();
-        for(int i=1;i<gather.length();i=i+2)
+    }
+
+    public boolean JudgeFun() {      //判断函数
+        int i=0;
+        for(i=0;i<A.size();i++)
         {
-            colEntity data2 =new colEntity();
-            data2.setData(Integer.parseInt(gather.substring(i,i+1)));
-            B.add(data2);
+            if(!JudgeExist(A.get(i).getData()))
+                break;
         }
-        System.out.println(B.get(0));
-        in.reset();
-        System.out.println("请输入A到B的关系:(输入格式为序偶集)");
-        gather=in.nextLine();
-        for(int i=2;i<gather.length();i=i+6)
-        {
-            binRelationEntity data =new binRelationEntity();
-            data.setOne(Integer.parseInt(gather.substring(i,i+1)));
-            data.setTwo(Integer.parseInt(gather.substring(i+2,i+3)));
-            BR.add(data);
+        return i
+    }
+
+    public boolean JudgeExist(int element) {   //判断A中元素在关系中是否都存在
+        int i = 0, j = 0;
+        for (j = 0; j < BR.size(); j++) {
+            if (element == BR.get(j).getOne())
+                break;
         }
-        System.out.println("<"+BR.get(0)+">");
+        return j != BR.size();
     }
 }
